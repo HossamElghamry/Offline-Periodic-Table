d5b1ea651a6f0afff1590d93a472027ea406b53d
HossamElghamry <ghamry_97@hotmail.com>
2019-04-16 10:39:52 +0200

    two groups

--- lib/element.dart
+++ lib/element.dart
@@ -8,9 +8,22 @@ class TableElement {
   final Color _color;
   final double _density;
   final double _ionizationEnergy;
+  final double _melting;
+  final double _boiling;
+  final String _appearance;
 
-  TableElement(this._symbol, this._name, this._massNum, this._atomicNum,
-      this._color, this._density, this._ionizationEnergy);
+  TableElement(
+    this._symbol,
+    this._name,
+    this._massNum,
+    this._atomicNum,
+    this._color,
+    this._density,
+    this._ionizationEnergy,
+    this._melting,
+    this._boiling,
+    this._appearance,
+  );
 
   String get symbol => _symbol;
   String get name => _name;
@@ -19,4 +32,7 @@ class TableElement {
   Color get color => _color;
   double get density => _density;
   double get energy => _ionizationEnergy;
+  double get melting => _melting;
+  double get boiling => _boiling;
+  String get appearance => _appearance;
 }
